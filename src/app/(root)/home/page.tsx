27d58cca0a251f36dd56e55544926bc2bfb2c0c1
Lý Hoài Lâm <hoailam03999@gmail.com>
2025-05-07 12:48:03 +0700

    fix home magrin-top

--- src/app/(root)/home/page.tsx
+++ src/app/(root)/home/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default function Home() {
 
     return (
-        <div className="mt-36 h-[2000px]">
+        <div className="mt-45 h-[2000px]">
             <AppContainer containerClassName="bg-[#1D3A52]" className="bg-transparent">
             <p className="text-amber-400">sorry i am currently creating a new website to use this domain, you can find its source code here: <Link href={"https://github.com/LyHoaiLam/bookstorenextjs"}>https://github.com/LyHoaiLam/bookstorenextjs</Link></p>
             <CarouselList />
